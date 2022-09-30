8852c4466ea5b7d37c0873d71209670a82f3d0d3
Angellyn218 <angellyncervantes@yahoo.com>
2022-09-30 14:45:01 -0700

    Created pseudocode for in js file

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// Add array with 10 words
+
+// button to start game
+//  event listener for click associated with id
+//  when click:
+//      - timer reset to 30
+//      - then choose random word
+
+
+// pick a random word for list
+//      hide certain letters
+// when called:
+//      - choose random word
+//      - choose random letters to show (letter status hidden and visible)
+//      - display word to user 
+//          - shown letters
+//          - hidden letters underscore
+//      - listen for key event
+
+// get user input (which keys they type)
+    // if key = next missing character, 
+    // display character/replace of underscore with proper letter
+// continue down path until word is completed or timer runs out
+// when type:
+//       - if key = next missing character
+//             - display character/replace of underscore with proper letter
+//      - when randomWord.length has all shown
+//             - add 1 to win
+//      - display high score
+
+// if user hits word before timer runs out, add 1 to win 
+//      if greater than stored high score, save time as score
+// else, add 1 to losses
+
+// timer 30 seconds (count down from 30) - display on screen
+//      if timer runs out, add to losses (local storage)
+
